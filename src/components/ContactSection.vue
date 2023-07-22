3ba833d0b6f5bb4f1327607e5a3dd01759daa4e9
arpado <ozsvar.arpad@gmail.com>
2023-07-22 13:07:02 +0200

    images not showing bugfix

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,42 +1,10 @@
 <template>
     <div id="clients" class="offset">
         <div class="jumbotron">
-            <!-- <div class="col-md-12 text-center">
-                <h3 class="heading">A Prima fogászat csapata</h3>
+            <div class="col-md-12 text-center">
+                <h3 class="heading">Itt találja Prima fogászat csapatát:</h3>
                 <div class="heading-underline"></div>
             </div>
-            <div class="row narrow">
-                <div class="col-md-6 client">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="../assets/images/client_4.jpg" />
-                        </div>
-                        <div class="col-md-8">
-                            <blockquote>
-                                <i class="fas fa-quote-left"></i>
-                                Igyekszem odaadóan, lelkiismeretesen és precízen ellátni a rám bízott feladatokat.
-                                <hr class="client-hr" />
-                                <cite>&#8212; Kis Pál, Fogorvos</cite>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 client">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="../assets/images/client_3.jpg" />
-                        </div>
-                        <div class="col-md-8">
-                            <blockquote>
-                                <i class="fas fa-quote-left"></i>
-                                Elkötelezett vagyok a kiemelkedő szintű fogászati ellátás nyújtásában
-                                <hr class="client-hr" />
-                                <cite>&#8212; Kovács Anna, Fogorvos</cite>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
             <iframe allowfullscreen="" aria-hidden="false" frameborder="0" height="600"
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2457.9970327219767!2d20.159241048239483!3d46.250062633888895!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe476cb973a27eef7!2sLiget%20Fog%C3%A1szati%20%C3%A9s%20Fogszab%C3%A1lyoz%C3%A1si%20Rendel%C5%91%20-%20Dr.%20V%C3%B6r%C3%B6s%20Laura%20Fogorvos%20Szeged!5e0!3m2!1sen!2shu!4v1604252891137!5m2!1sen!2shu"
                 style="border:0;" tabindex="0" width="100%"></iframe>
--- src/components/StaffSection.vue
+++ src/components/StaffSection.vue
@@ -9,7 +9,7 @@
                 <div class="col-md-6 doctor" v-for="(doctor, index) in doctors" :key="index">
                     <div class="doctor-card">
                         <div class="col-md-4 doctor-card-image-container">
-                            <img :src="`/src/assets/images/${doctor.pic}`" />
+                            <img :src="`../../src/assets/images/${doctor.pic}`" />
                         </div>
                         <div class="col-md-8">
                             <blockquote>
