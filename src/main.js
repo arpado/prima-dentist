2c959f90173ea1aee6198059c67b3ba5e3c8e18f
arpado <ozsvar.arpad@gmail.com>
2023-07-23 17:11:31 +0200

    i18n added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n';
 import App from './App.vue'
+import en from './locales/en.json'
+import hu from './locales/hu.json'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    legacy: false, // Set to false to use the Composition API
+    locale: 'en', // Set the default locale
+    fallbackLocale: 'en', // Set the fallback locale
+    messages: {
+      en, // Import English translations
+      hu // Import Hungarian translations
+    }
+  });
+
+const app = createApp(App)
+app.use(i18n);
+app.mount('#app')
